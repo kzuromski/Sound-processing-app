a56dfc92887f9f192999b58deae3f546eecf627b
Queslan <sobota96@wp.pl>
2017-10-16 22:47:10 +0200

    v1.1
    Struktury usunięte, sam plik wstępnie działa, całość jest czysto do sprawdzenia funkcjonalności.

--- entropy/WaveReader/WaveReader.cpp
+++ entropy/WaveReader/WaveReader.cpp
@@ -10,159 +10,115 @@
 #include "mmsystem.h"
 #include <fstream>
 #include <time.h>
+#include <vector>
 
 using namespace std;
-
-struct wave_header
-{
-	char chunkID[4]; //ID RIFF
-	unsigned long chunkSize; //rozmiar danych
-	char format[4]; //format pliku (WAV)
-	char subchunk1ID[4];  //pocz¹tek czêœci opisowej (fmt)
-	unsigned long subchunk1Size; //rozmiar czêœci opisowej, dla fmt zwykle 16
-	unsigned short audioFormat; //rodzaj kompresji, 1 - bez kompresji, modulacja PCM
-	unsigned short numChannels; //liczba kana³ów
-	unsigned long sampleRate; //czêstotliwoœæ
-	unsigned long byteRate; //czêstotliwoœæ bajtów, czêstotliwoœæ * liczba kana³ów * rozdzielczoœæ / 8
-	unsigned short blockAlign; //rozmiar próbki, liczba kana³ów * rozdzielczoœæ / 8
-	unsigned short bitsPerSample; //rozdzielczoœæ w bitach
-};
-
-struct wave_chunk
-{
-	char ID[4]; //ID danych, pocz¹tek czêœci z danymi
-	unsigned long size; //rozmiar bloku danych
-};
 
 class WaveReader
 {
 private:
-	wave_header header; //blok RIFF
-	FILE *fin; //plik wav
-	ofstream output; //plik wynikowy
-	wave_chunk chunk; //blok data
-	int sampleSize; //rozmiar sampla
-	int sampleCount; //iloœæ sampli w pliku
-	int N; //iloœæ sampli w kanale
-	short int *Lchannel; //lewy kana³
-	short int *Rchannel; //prawy kana³
-	_int16 XavgSqr; //œrednia kwadratowa dla danych podstawowych
-	_int16 EavgSqr; //œrednia kwadratowa dla danych ró¿nicowych
-	_int16 Xentropy; //H(s) dla danych podstawowych
-	_int16 Eentropy; //H(s) dla danych ró¿nicowych
-	tm time;
+	FOURCC riff; // "RIFF" file descripton header
+	DWORD size_of_file; // size of file
+	FOURCC wave; // "WAVE" file descripton header
+	FOURCC fmt; // "fmt" description header
+	DWORD chunk; // size of WAVE section chunck
+	WORD pcm; // WAVE type format
+	WORD chanel; // mono/stereo
+	DWORD sample_rate; // sample rate
+	DWORD bytes_per_sec; // bytes/sec
+	WORD block_alignment; // Block alignment
+	WORD bits_per_sample; // Bits/sample
+	FOURCC data; // "data" description header
+	DWORD size_of_data; // size of data chunk
+	INT16 data_of_file;
+	vector<INT16> left;
+	vector<INT16> right;
+	ofstream new_file;
+	FILE *wf;
+	double ammount_of_samples;
 public:
-	WaveReader(const char *FileName)
+	WaveReader(string name_of_wave)
 	{
 		
-		fin = fopen(FileName, "rb");
-		fread(&header, sizeof(header), 1, fin);
-		while (true)
+		const char * c = name_of_wave.c_str();
+		wf = fopen(c, "r");
+
+		if (wf == NULL)
 		{
-			fread(&chunk, sizeof(chunk), 1, fin);
-			if (*(unsigned int *)&chunk.ID == 0x61746164)
-			{
-				break;
-			}
+			cout << "File is not opened" << endl;
+			exit(-1);
 		}
-		sampleSize = header.bitsPerSample / 8;
-		sampleCount = chunk.size * 8 / header.bitsPerSample;
-		N = sampleCount / 2;
-		Lchannel = new short int[N];
-		Rchannel = new short int[N];
-		DisplayInformation(FileName);
-		string FileNameOut;
-		FileNameOut = "Result_" + to_string(time.tm_mday) + to_string(time.tm_mon) + to_string(time.tm_year) + to_string(time.tm_hour);
-		output.open(FileNameOut + ".txt");
-		output << "Nazwa\tŒrednia kwadratowa X\tŒrednia kwadratowa E\tEntropia X\tEntropia E\n";
+
+		SYSTEMTIME time;
+		GetLocalTime(&time);
+		string file_name;
+		file_name = to_string(time.wHour) + "_" + to_string(time.wMinute) + "_" + to_string(time.wSecond);
 		ReadData();
-		FinalValues();
-		WriteToFile(FileName);
+
+		ofstream new_file(file_name + ".txt");
+		double ammount_of_samples = size_of_data / 4;
+		new_file << name_of_wave << endl;
+		new_file << "Size of data: " << size_of_data << endl;
+		new_file << "Ammount of samples: " << ammount_of_samples << endl;
+		calculations();
+		new_file << "First calculation of left side: " << first_calculation(ammount_of_samples / 2, left) << endl;
+		new_file << "First calculation of right side: " << first_calculation(ammount_of_samples / 2, right) << endl;
 
 	}
 	 
 	~WaveReader()
 	{
-		fclose(fin);
-		output.close();
-	}
-
-	void DisplayInformation(char const *FileName) //wyœwietlanie danych
-	{
-		cout << "************WAV file properties************" << endl;
-		cout << "File name: " << FileName << endl;
-		cout << "File type: " << header.chunkID << endl;
-		cout << "File format: " << header.format << endl;
-		cout << "File size: " << header.chunkSize << endl;
-		cout << "Format name: " << header.subchunk1ID << endl;
-		cout << "Format length: " << header.subchunk1Size << endl;
-		cout << "Conversion type: " << header.audioFormat << endl;
-		cout << "Number of channels: " << header.numChannels << endl;
-		cout << "Sample rate: " << header.sampleRate << endl;
-		cout << "Bits per sample: " << header.bitsPerSample << endl;
+		new_file.close();
 	}
 
 	void ReadData() //czytanie wartoœci próbek do wektorów kana³ów
 	{
-		int n = 0, k = 0;
-		for (int i = 0; i < sampleCount; i++)
+		fread(&riff, sizeof(FOURCC), 1, wf);
+		fread(&size_of_file, sizeof(DWORD), 1, wf);
+		fread(&wave, sizeof(FOURCC), 1, wf);
+		fread(&fmt, sizeof(FOURCC), 1, wf);
+		fread(&chunk, sizeof(FOURCC), 1, wf);
+		fread(&pcm, sizeof(WORD), 1, wf);
+		fread(&chanel, sizeof(WORD), 1, wf);
+		fread(&sample_rate, sizeof(DWORD), 1, wf);
+		fread(&bytes_per_sec, sizeof(DWORD), 1, wf);
+		fread(&block_alignment, sizeof(WORD), 1, wf);
+		fread(&bits_per_sample, sizeof(WORD), 1, wf);
+		fread(&data, sizeof(FOURCC), 1, wf);
+		fread(&size_of_data, sizeof(DWORD), 1, wf);
+	}
+
+	
+	void calculations()
+	{
+		ammount_of_samples = size_of_data / 4;
+		for (int i = 0; i < ammount_of_samples; i++)
 		{
-			if (i % 2 == 0) //parzyste próbki to kana³ lewy, próbki s¹ w pliku u³o¿one na przemian lewy kana³ prawy kana³ itd.
+			fread(&data_of_file, sizeof(INT16), 1, wf);
+			if (i % 2 == 0)
 			{
-				fread(&Lchannel[n], sampleSize, 1, fin);
-				n++;
+				left.push_back(data_of_file);
 			}
 			else
 			{
-				fread(&Rchannel[k], sampleSize, 1, fin);
-				k++;
+				right.push_back(data_of_file);
 			}
 		}
+
+		
 	}
 
-	short int *Differental(short int *channel) //ró¿nicowanie kana³u
+	double first_calculation(double a, vector<INT16> b)
 	{
-		short int *differentalChannel = new short int[N];
-		differentalChannel[0] = channel[0];
-		for (int i = 1; i < N ; i++)
+		double full = 0;
+		for (double i = 0; i < a; i++)
 		{
-			differentalChannel[i] = channel[i] - channel[i - 1];
-		}
-		return differentalChannel;
-	}
-
-	_int16 Calculations(short int *channel, int option)
-	{
-		_int16 result = 0;
-		_int16 avg = 0;
-		switch (option)
-		{
-		case 1: //œrednia kwadratowa dla danych podstawowych
-			for (int i = 0; i < N - 1; i++)
-			{
-				avg = avg + (_int16)pow(channel[i],2);
-			}
-			result = (avg / N);
-			break;
-
-		default:
-			cout << "Niepoprawna opcja" << endl;
-			result = -1;
-			break;
+			full = (double)(full + (b.at(i) * b.at(i)));
 		}
 
-		return result;
-	}
+		full = full / a;
 
-	void FinalValues() //liczenie ostatecznych wartoœci
-	{
-		XavgSqr = 1/2 * (Calculations(Lchannel, 1) + Calculations(Rchannel, 1));
-		EavgSqr = 1 / 2 * (Calculations(Differental(Lchannel), 1) + Calculations(Differental(Rchannel), 1));
-	}
-
-	void WriteToFile(const char *FileName) // zapisywanie wyników do txt
-	{
-		output << FileName << "\t" << XavgSqr << "\t" << EavgSqr <</* "\t" << Xentropy << "\t" << Eentropy << */endl;
+		return full;
 	}
 
 };
@@ -170,5 +126,4 @@ public:
 void main()
 {
 	WaveReader wave("gun.wav");
-	getchar();
 }
