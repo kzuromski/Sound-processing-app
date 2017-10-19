a90d31903122da477174d274f3a0fae8915530fb
dolan2 <krawczyk@post.com>
2017-10-19 09:41:26 +0200

    ROWNANIE
    dodana metoda LU

--- entropy/WaveReader/WaveReader.cpp
+++ entropy/WaveReader/WaveReader.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -40,6 +41,8 @@ private:
 	ofstream new_file;
 	FILE *wf;
 	double ammount_of_samples;
+	const double eps = 1e-12; //wspolczynnik
+
 public:
 	WaveReader(string name_of_wave)
 	{
@@ -208,11 +211,104 @@ public:
 		}
 	}*/
 
+	// Funkcja dokonuje rozk³adu LU macierzy A
+	bool ludist(int n, double ** A)
+	{
+		int i, j, k;
 
+		for (k = 0; k < n - 1; k++)
+		{
+			if (fabs(A[k][k]) < eps) return false;
+
+			for (i = k + 1; i < n; i++)
+				A[i][k] /= A[k][k];
+
+			for (i = k + 1; i < n; i++)
+				for (j = k + 1; j < n; j++)
+					A[i][j] -= A[i][k] * A[k][j];
+		}
+
+		return true;
+	}
+
+	// Funkcja wyznacza wektor X na podstawie A i B
+	bool lusolve(int n, double ** A, double * B, double * X)
+	{
+		int    i, j;
+		double s;
+
+		X[0] = B[0];
+
+		for (i = 1; i < n; i++)
+		{
+			s = 0;
+
+			for (j = 0; j < i; j++) s += A[i][j] * X[j];
+
+			X[i] = B[i] - s;
+		}
+
+		if (fabs(A[n - 1][n - 1]) < eps) return false;
+
+		X[n - 1] /= A[n - 1][n - 1];
+
+		for (i = n - 2; i >= 0; i--)
+		{
+			s = 0;
+
+			for (j = i + 1; j < n; j++) s += A[i][j] * X[j];
+
+			if (fabs(A[i][i]) < eps) return false;
+
+			X[i] = (X[i] - s) / A[i][i];
+		}
+
+		return true;
+	}
+
+	void SystemOfEquations() {
+		double **A, *B, *X;
+		int n, i, j;
+
+		cout << setprecision(4) << fixed;
+
+		cout << "PODAJ N" << endl;
+		cin >> n;
+
+		// tworzymy macierze A, B i X
+		A = new double *[n];
+		B = new double[n];
+		X = new double[n];
+
+		for (i = 0; i < n; i++) A[i] = new double[n];
+
+		// odczytujemy dane dla macierzy A i B
+		cout << "Podaj A i B" << endl;
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++) cin >> A[i][j];
+			cin >> B[i];
+		}
+
+		// rozwi¹zujemy uk³ad i wyœwietlamy wyniki
+		if (ludist(n, A) && lusolve(n, A, B, X))
+		{
+			for (i = 0; i < n; i++) cout << "x" << i + 1 << " = " << setw(9) << X[i] << endl;
+		}
+		else cout << "DZIELNIK ZERO\n";
+
+		// usuwamy macierze z pamiêci
+		for (i = 0; i < n; i++) delete[] A[i];
+		delete[] A;
+		delete[] B;
+		delete[] X;
+	}
 
 };
 
 void main()
 {
 	WaveReader wave("sample.wav");
+	wave.SystemOfEquations();
+	system("pause");
 }
